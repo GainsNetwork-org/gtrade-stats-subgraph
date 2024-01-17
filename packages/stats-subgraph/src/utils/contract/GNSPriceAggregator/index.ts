b1443095446c2ed7e6cf12346e9df2074dc868e4
thebuidler <thebuidler@pm.me>
2024-01-16 21:26:25 -0800

    Updated PriceAggregator ABI and fetching of collateralPriceUsd

--- packages/stats-subgraph/src/utils/contract/GNSPriceAggregator/index.ts
+++ packages/stats-subgraph/src/utils/contract/GNSPriceAggregator/index.ts
@@ -1,7 +1,10 @@
 import { Address, BigDecimal, log } from "@graphprotocol/graph-ts";
 import { GNSPriceAggregator } from "../../../types/GNSTradingCallbacksV6_4_1/GNSPriceAggregator";
 import { IChainlinkFeed } from "../../../types/GNSTradingCallbacksV6_4_1/IChainlinkFeed";
-import { getNetworkCollateralAddresses, getMultiCollatBlock } from "../../constants";
+import {
+  getNetworkCollateralAddresses,
+  getMultiCollatBlock,
+} from "../../constants";
 
 export function getPriceAggregatorContract(
   network: string,
@@ -9,42 +12,37 @@ export function getPriceAggregatorContract(
   blockNumber: i32
 ): GNSPriceAggregator {
   const config = getNetworkCollateralAddresses(network, collateral);
-  const multiCollatBlock = getMultiCollatBlock(network)
+  const multiCollatBlock = getMultiCollatBlock(network);
   if (config == null) {
     throw new Error("Network not supported");
   }
-  if(blockNumber > multiCollatBlock) {
-    return GNSPriceAggregator.bind(Address.fromString(config.gnsPriceAggregator));
-  }
-  else {
-    return GNSPriceAggregator.bind(Address.fromString(config.gnsPriceAggregator_Old));
+  if (blockNumber > multiCollatBlock) {
+    return GNSPriceAggregator.bind(
+      Address.fromString(config.gnsPriceAggregator)
+    );
+  } else {
+    return GNSPriceAggregator.bind(
+      Address.fromString(config.gnsPriceAggregator_Old)
+    );
   }
 }
 
 // For backwards compatibility this function returns 1 if the price feed is not found
-// @todo once multicollat is deployed to mainnet, this function should throw an error
 export function getCollateralPrice(
   network: string,
   collateral: string,
   blockNumber: i32
 ): BigDecimal {
-  const priceAggregator = getPriceAggregatorContract(network, collateral,blockNumber);
-  const priceFeed = priceAggregator.try_collateralPriceFeed();
-  if (priceFeed.reverted) {
+  const priceAggregator = getPriceAggregatorContract(
+    network,
+    collateral,
+    blockNumber
+  );
+  const collateralPriceUsd = priceAggregator.try_getCollateralPriceUsd();
+  if (collateralPriceUsd.reverted) {
     // log.warn("Price feed not found");
     return BigDecimal.fromString("1");
   }
-  const feed = IChainlinkFeed.bind(priceFeed.value);
-  const price = feed.try_latestRoundData();
-  if (price.reverted) {
-    // log.warn("Latest round data missing");
-    return BigDecimal.fromString("1");
-  }
-
-  const precision = priceAggregator.try_getCollateralPrecision();
-
-  return price.value
-    .getValue1()
-    .toBigDecimal()
-    .div(precision.value.toBigDecimal());
+  const price = collateralPriceUsd.value;
+  return price.toBigDecimal().div(BigDecimal.fromString(1e8 + ""));
 }
