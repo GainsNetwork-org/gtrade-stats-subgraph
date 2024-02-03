30fead9133ab802d67cc1adb5a3044624f3c3ba1
thebuidler <thebuidler@pm.me>
2024-02-02 20:54:28 -0600

    Updated all subgraphs to new versions

--- packages/graph-client/helpers/config.ts
+++ packages/graph-client/helpers/config.ts
@@ -1,11 +1,11 @@
 export const GTOKEN_SUBGRAPH = {
-  137: "gtoken-polygon",
-  42161: "gtoken-arbitrum",
-  80001: "gtoken-mumbai",
+  137: "gtoken-polygon-mc1",
+  42161: "gtoken-arbitrum-mc1",
+  80001: "gtoken-mumbai-mc1",
 };
 
 export const STATS_SUBGRAPH = {
-  137: "gtrade-stats-polygon",
-  42161: "gtrade-stats-arbitrum",
-  80001: "gtrade-stats-mumbai",
+  137: "gtrade-stats-polygon-v2",
+  42161: "gtrade-stats-arbitrum-v2",
+  80001: "gtrade-stats-mumbai-v2",
 };
