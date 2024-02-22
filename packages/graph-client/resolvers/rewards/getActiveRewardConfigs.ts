266f3ed2a18fe4f4dd7d5a8bc7b2a70878e686c4
thebuidler <thebuidler@pm.me>
2024-02-22 10:52:18 -0800

    Added additional reward distributions

--- packages/graph-client/resolvers/rewards/getActiveRewardConfigs.ts
+++ packages/graph-client/resolvers/rewards/getActiveRewardConfigs.ts
@@ -43,7 +43,7 @@ export const ARBITRUM_STIP_REWARDS = {
     },
     {
       startEpoch: 10,
-      endEpoch: 12,
+      endEpoch: 11,
       rewardDistribution: {
         loyalty: 0.1,
         fee: 0.5,
@@ -74,11 +74,33 @@ export const MUMBAI_STIP_REWARDS_TEST = {
   rewardDistributionOverrides: [
     {
       startEpoch: 6,
-      endEpoch: 15,
+      endEpoch: 7,
       rewardDistribution: {
         loyalty: 0.1,
         fee: 0.775,
         absSkill: 0.125,
+        relSkill: 0.0,
+        diversity: 0.0,
+      },
+    },
+    {
+      startEpoch: 8,
+      endEpoch: 9,
+      rewardDistribution: {
+        loyalty: 0.0,
+        fee: 1,
+        absSkill: 0.0,
+        relSkill: 0.0,
+        diversity: 0.0,
+      },
+    },
+    {
+      startEpoch: 10,
+      endEpoch: 12,
+      rewardDistribution: {
+        loyalty: 0.1,
+        fee: 0.5,
+        absSkill: 0.4,
         relSkill: 0.0,
         diversity: 0.0,
       },
