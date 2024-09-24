5efb73e99005c5c9bf6b2c1e0f761032afcc5de0
thebuidler <thebuidler@pm.me>
2024-09-24 14:47:17 -0700

    Bumped vs for Base gToken subgraph

--- packages/graph-client/helpers/config.ts
+++ packages/graph-client/helpers/config.ts
@@ -3,6 +3,7 @@ export const GTOKEN_SUBGRAPH = {
   42161: "J9WvpchpS1Sd5fQ4t7grJgMeC3nFTgHuJJe88YnkoaPF",
   80001: "",
   421614: "",
+  8453: "Afnwe81PTY5Rf1wHyGafPMWbKoDn1HBNamBMLYHQucr4",
 };
 
 export const STATS_SUBGRAPH = {
@@ -10,4 +11,5 @@ export const STATS_SUBGRAPH = {
   42161: "HSP3yDXzM5pWeBfFAADPLYu6RFYwo9vg5zdpMMmQeVpe",
   80001: "",
   421614: "",
+  8453: "",
 };
